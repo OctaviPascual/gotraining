3ee701af72bef39c026acfd51eca3262dd72d940
Octavi Pascual <octavi94@gmail.com>
2022-09-13 08:55:38 +0200

    Complete encoding exercise

--- topics/go/packages/encoding/exercises/template1/template1.go
+++ topics/go/packages/encoding/exercises/template1/template1.go
@@ -8,21 +8,52 @@
 // Marshal the slice into pretty print strings and display each element.
 package main
 
-// Add imports.
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 // Declare a struct type named user with two fields. Name of type string and
 // Email of type string. Add tags for each field for the unmarshal call.
+type user struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
 
 func main() {
 
 	// Use the os package to Open the JSON file. Check for errors.
+	file, err := os.Open("data.json")
+	if err != nil {
+		fmt.Println("Could not open file:", err)
+		return
+	}
 
 	// Schedule the file to be closed once the function returns.
+	defer file.Close()
 
 	// Declare a nil slice of user struct types.
+	var users []user
+
 	// Decode the JSON from the file into the slice. Check for errors.
+	err = json.NewDecoder(file).Decode(&users)
+	if err != nil {
+		fmt.Println("Could not decode file:", err)
+		return
+	}
 
 	// Iterate over the slice and display each user value.
+	for _, u := range users {
+		fmt.Printf("%+v\n", u)
+	}
 
 	// Marshal each user value and display the JSON. Check for errors.
+	marshaledUsers, err := json.MarshalIndent(&users, "", "    ")
+	if err != nil {
+		fmt.Println("Could not marshal users:", err)
+		return
+	}
+
+	fmt.Println(string(marshaledUsers))
 }
